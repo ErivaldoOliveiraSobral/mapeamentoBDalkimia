1b5e589fbc44e6cfeb9cd250218509ff5f275103
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-04-29 14:15:32 -0300

    Tabelas de inserção

---
+++ INSERT-ws_Endereco.sql
@@ -0,0 +1,14 @@
+INSERT INTO ws_Endereco(id, end_linha_1, end_linha_2, cidade, cep, pais, estado, id_usuario)
+VALUES
+(64,'Rua João Antônio de Oliveira','Mooca','São Paulo','03111-001','Brasil','São Paulo',64),
+(1,'Rua Ytaipu, 255 APT31','Mirandópolis','São Paulo','04052-010','Brasil','São Paulo',1),
+(62,'','','','','','',62),
+(60,'','','','','','',60),
+(46,'Rua Marília, 486','Vila Bartira','Itaquaquecetuba','08577-560','Brasil','São Paulo',46),
+(58,'Rua Maria Santíssima, 10','Cidade Nova Heliópolis','São Paulo','04236-310','Brasil','São Paulo',58),
+(56,'Rua Dom Meinrado, 177','Jardim Monjolo','São Paulo','02961-140','Brasil','São Paulo',56),
+(61,'Rua Nazare, 109 sala 18','Vargem grande paulista','São Paulo','06730-000','Brasil','São Paulo',61),
+(65,'Rua Marília','Vila Bartira','São Paulo','08577-560','Brasil','São Paulo',65),
+(48,'Rua Ytaipu, 255 APT31','Mirandópolis','São Paulo','04052-010','Brasil','São Paulo',48),
+(59,'Rua Coronel Homero da Silveira, 247','Parque Peruche','São Paulo','02535-050','Brasil','São Paulo',59),
+(63,'Rua Desidério Ferreira, 12','Parque Pinheiros','São Paulo','06767-270','Brasil','São Paulo',59)
