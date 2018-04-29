ab378120cd4e66e846c6d68f902f459ae5250480
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-04-29 14:37:41 -0300

    insert usuario

---
+++ INSERT-ws_Usuario.sql
@@ -0,0 +1,13 @@
+INSERT INTO ws_Usuario(id,nome,sobrenome,email,telefone) VALUES
+(64,'Fabio','fuRIA','10furia@gmail.com','11993179484'),
+(62,'carla_gamaalves','','carla_gamaalves@yahoo.com.br',''),
+(60,'danilostefano','','danilostefano@outlook.com',''),
+(46,'erivaldooliveirasobral','','erivaldooliveirasobral@gmail.com',''),
+(58,'Fabio','Santos','fabio.rogerio@alstomgroup.com','11-20618112'),
+(56,'Felipe Matheus','silva','felipe-ny1@hotmail.com','11959851665'),
+(61,'Flavia','Fukutaki','flavia.fukutaki@gmail.com','11984989648'),
+(65,'Matheus','silva','matheus-silva@hotmail.com','11959851665'),
+(48,'EDSON','TAKANOHASHI','miyuki.kamata@gmail.com','11997776858'),
+(59,'Roberto Takeshi','Maruya','roberto.maruya@gmail.com','11983443714'),
+(63,'vIVIAN','sINJO','vi.tieme@gmail.com','11976847458'),
+(1,'KAREN MIYUKI KAMATA','TAKANOHASHI','alkimia.simplesassim@gmail.com','11997776858')
